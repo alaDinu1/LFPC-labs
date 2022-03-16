c5c3d18ba092f924a0f736f3ce81f427e9a44375
alaDinu1 <76076900+alaDinu1@users.noreply.github.com>
2022-03-16 14:05:12 +0200

    lab 2 commit

---
+++ main.java
@@ -0,0 +1,10 @@
+package lab2;
+
+public class main {
+    public static void main(String[] args) {
+        Auto NFA = new Auto();
+        NFA.inputAuto();
+        NFA.printAuto();
+        NFA.toDFA();
+    }
+}
